e715ec56ab171d948083734faec0f407820d5700
Chris Santamaria <csantamaria@intershop.com>
2025-05-07 15:10:16 +1000

    feat: updated bootstrap version, updated jest testing

--- src/app/shared/components/product/product-image/product-image.component.spec.ts
+++ src/app/shared/components/product/product-image/product-image.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { of } from 'rxjs';
 import { anything, instance, mock, when } from 'ts-mockito';
